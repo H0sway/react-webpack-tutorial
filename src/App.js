37b791d3f18ad6cd19a0128bbdb6702651502f33
Joshua Russell <jkrussell756@gmail.com>
2019-01-16 12:27:28 -0500

    Wrote a basic component in App to be rendered by index

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// import React
+import React, { Component } from 'react';
+
+// Define the App component
+class App extends Component {
+  // Render the HTML that's returned in this method
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello there!</h1>
+      </div>
+    )
+  }
+};
+
+// Export App
+export default App;
